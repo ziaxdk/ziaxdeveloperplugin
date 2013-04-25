c1cb10020b9e5a446558dd129c76418f7df5e902
ziaxdk <ziaxdk@gmail.com>
2013-04-25 11:08:22 +0200

    settings dialog

--- js/ziax-app.js
+++ js/ziax-app.js
@@ -2,7 +2,51 @@ angular.module("ziax-app", [])
 
 
 .run(function() {
-    
 
-alert("ok");
-});
+})
+
+.controller("home", ["$scope", "Storage", function($scope, Storage){
+    var settings;
+
+    settings = Storage.get();
+    if (!settings) $("#showSettingsModel").modal();
+
+}])
+
+.controller("settings", ["$scope", "Storage", function($scope, Storage){
+    $scope.form = {};
+    $scope.submit = function() {
+        Storage.set({apiKey: $scope.form.apiKey, database: $scope.form.database});
+        $("#showSettingsModel").modal("hide");
+    }
+}])
+
+
+
+.controller("test", ["$scope", "Storage", function($scope, Storage){
+
+    $scope.get = function() {
+         console.log(Storage.get());
+    }
+
+    $scope.set = function() {
+            Storage.set({foo: "bar"});
+    }
+    $scope.del = function() {
+            Storage.clear();
+    }
+
+
+}])
+
+
+.service("Storage", [function(){
+    var storage = chrome.storage.local;
+    
+    return {
+        get: function(){ storage.get(function(items){ return items;  }); },
+        set: function(obj){ storage.set(obj); },
+        clear: function() { storage.clear(); }
+    }
+}])
+;
